d8faa4d8c8d3b47239e83d745c0e4e5da15d8202
= <=>
2023-12-02 18:42:30 +0500

    python methods done

---
+++ python/filter.py
@@ -0,0 +1,14 @@
+
+
+arr : list[int] = [1,2,3,4,5,6,7,8,9,10]
+def func(element):
+   # return element%2==0
+   if(element%2==0):
+      return element 
+   else:
+      return False
+
+filtered_arr : list[int] = list(filter(func,arr))
+filtered_arr1 : list[int] = list(filter(lambda element : element%2==0,arr))
+print(filtered_arr)
+print(filtered_arr1)
